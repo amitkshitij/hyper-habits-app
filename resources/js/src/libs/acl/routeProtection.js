fa54535dabbfac5e134cd749d2b856a43e2e56a8
Amit Sinha <amitsinhadeveloper@gmail.com>
2026-06-13 14:32:52 +0530

    fix: allow routes without resource meta to bypass ACL check - fixes not-authorized redirect on dashboard refresh

--- resources/js/src/libs/acl/routeProtection.js
+++ resources/js/src/libs/acl/routeProtection.js
@@ -1,5 +1,9 @@
 import ability from './ability'
 
-export const canNavigate = to => to.matched.some(route => ability.can(route.meta.action || 'read', route.meta.resource))
+export const canNavigate = to => to.matched.some(route => {
+  // If no resource is defined on route meta, allow access (treat as public/unprotected)
+  if (!route.meta || !route.meta.resource) return true
+  return ability.can(route.meta.action || 'read', route.meta.resource)
+})
 
 export const _ = undefined
